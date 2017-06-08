5ea299ef9ace0720e1216811e76b7e6c0db55abb
Dave Sims <misc@lamneth.net>
2017-06-08 07:37:26 -0400

    Added some new functionality and fixed the path requirements

--- bin/matchbox_json_dump.py
+++ bin/matchbox_json_dump.py
@@ -7,9 +7,11 @@ from pprint import pprint as pp
 
 # Dirty hack for right now to see if we can figure out how call this as an external script
 # TODO: Fix this once we make a real package.
-bin_path = os.path.dirname(os.path.realpath(__file__)).rstrip('/bin')
-sys.path.append(bin_path)
-from Matchbox import *
+# bin_path = os.path.dirname(os.path.realpath(__file__)).rstrip('/bin')
+# sys.path.append(bin_path)
+from matchbox_api_utils.Matchbox import *
+
+version = '1.3.0_060717'
 
 class Config(object):
     def __init__(self,config_file):
@@ -33,8 +35,9 @@ class Config(object):
             data = json.load(fh)
         return data
 
+
 def get_args():
-    version = '1.2.0_110116'
+
     parser = argparse.ArgumentParser(
         formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position=100, width=150),
         description=
@@ -43,24 +46,54 @@ def get_args():
         periodic searching for data.  
         '''
     )
+    parser.add_argument('-d', '--data', metavar='<raw_mb_datafile.json>',
+            help='Load a raw MATCHBox database file (usually after running with the -r option).')
+    parser.add_argument('-r', '--raw', action='store_true',
+        help='Generate a raw dump of MATCHbox so that we can see the raw data structure available for debugging '
+              'and dev purposes.')
+    parser.add_argument('-o', '--outfile', metavar='<out.json>', 
+            help='Name of output JSON file. DEFAULT: "mb_<datestring>.json"')
     parser.add_argument('-v', '--version', action='version', version = '%(prog)s  -  ' + version)
-    parser.add_argument('-o', '--outfile', metavar='<out.json>', help='Name of output JSON file. DEFAULT: "mb_<datestring>.json"')
     args = parser.parse_args()
     return args
 
 def main(data,outfile=None):
-    print('args as passed\ndata: {}\noutfile: {}\n'.format(data,outfile))
+    # print('args as passed\ndata: {}\noutfile: {}\n'.format(data,outfile))
     sys.stdout.write("Dumping matchbox as a JSON file for easier and faster code testing...")
     sys.stdout.flush()
     data._matchbox_dump(outfile)
     sys.stdout.write("Done!")
 
 if __name__=='__main__':
+    if os.path.isfile(os.path.join(os.getcwd(), '/config.json')):
+        config_file = os.path.join(os.getcwd(), '/config.json')
+    else:
+        config_file = os.path.join(os.environ['HOME'], '.mb_utils/config.json')
+    
     # config_file = 'config.json'
-    config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.json')
-    config_data = Config.read_config(config_file)
+    # config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.json')
+    try:
+        config_data = Config.read_config(config_file)
+    except IOError:
+        print('ERROR: No configuration file found. Need to create a config file int he current direcotry or use '
+              'the system provided one in ~/.mb_utils/config.json')
+        sys.exit(1)
+
     args = get_args()
+    if args.raw:
+        MatchboxData(config_data['url'],config_data['creds'],raw_dump=True)
+        sys.exit()
+
     outfile = args.outfile
-    data = MatchboxData(config_data['url'],config_data['creds'])
+    sys.stdout.write('Retrieving MATCHBox dataset for DB obj creation...\n')
+    sys.stdout.flush()
+
+    if args.data:
+        data = MatchboxData(config_data['url'],config_data['creds'],dumped_data=args.data)
+    else:
+        # data = MatchboxData(config_data['url'],config_data['creds'])
+        data = MatchboxData(config_data['url'],config_data['creds'],patient=16271)
+
+    sys.stdout.write('Done!')
     main(data,outfile)
 
