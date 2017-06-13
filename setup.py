2fa7da696285b5a1373ca93a67452d819e76b213
Dave Sims <misc@lamneth.net>
2017-06-13 09:18:59 -0400

    Updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+import sys, os
 try:
     from setuptools import setup
 except ImportError:
@@ -8,12 +8,14 @@ def readme():
     with open('README.rst') as fh:
         return fh.read()
 
+version = '0.9.14a1'
+
 config = {
     'name'                   : 'matchbox_api_utils',
-    'description'            : ('MATCHBox API utlilites package to retrieve and analyze variant, patient, and disease information'
+    'description'            : ('MATCHBox API utlilites package to retrieve variant, patient, and disease information'
                                 'from MATCHBox in a quick and informative way.'),
     'long_description'       : readme(),
-    'version'                : '0.9.13a1',
+    'version'                : version,
     'author'                 : 'Dave Sims',
     'author_email'           : 'simsdj@mail.nih.gov',
     'download_url'           : 'https://github.com/drmrgd/matchbox_api_utils.git',
@@ -23,6 +25,9 @@ config = {
     'packages'               : ['matchbox_api_utils'],
     'scripts'                : ['bin/map_msn_psn.py',
                                 'bin/matchbox_json_dump.py'
+                                'bin/match_specimen_tracking.py'
+                                'bin/match_variant_frequency.py'
+                                'bin/matchbox_patient_summary.py'
                                ],
     'data_files'             : [(os.environ['HOME']+'/.mb_utils/', ['config.json'])],
     'include_package_data'   : True,
