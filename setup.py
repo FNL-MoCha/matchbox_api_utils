c7041a35282a8df1201bcbe74647c2dbe845a1e8
Dave Sims <misc@lamneth.net>
2017-06-21 15:48:13 -0400

    Fully implemented the post installer scripts and functions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys, os
 from setuptools import setup
 from distutils.command.install import install as _install
 
-version = '0.9.16a1'
+version = '0.9.17a1'
 
 class install(_install):
     def run(self):
@@ -13,7 +13,6 @@ class install(_install):
         
 def _post_install(dir):
     from subprocess import call
-    #call([sys.executable, 'postinstall.py'], cwd=os.path.join(dir, 'script_folder'))
     call([sys.executable, 'postinstall.py'])
 
 def readme():
@@ -22,8 +21,7 @@ def readme():
 
 config = {
     'name'                   : 'matchbox_api_utils',
-    'description'            : ('MATCHBox API utlilites package to retrieve variant, patient, and disease information'
-                                'from MATCHBox in a quick and informative way.'),
+    'description'            : ('MATCHBox API Utlilites Package'),
     'long_description'       : readme(),
     'version'                : version,
     'author'                 : 'Dave Sims',
@@ -41,7 +39,6 @@ config = {
                                 'bin/match_variant_frequency.py',
                                 'bin/matchbox_patient_summary.py',
                                ],
-    'data_files'             : [(os.environ['HOME']+'/.mb_utils/', ['config.json'])],
     'include_package_data'   : True,
     'zip_safe'               : False,
     'license'                : 'MIT',
