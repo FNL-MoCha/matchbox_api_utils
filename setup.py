2e9046154b2ec1fdf71789d4c2cf8f6b3133ad22
Dave Sims <misc@lamneth.net>
2017-07-18 09:51:09 -0400

    Updated

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 import sys, os
 from setuptools import setup
-# from distutils.command.install import install as _install
 from setuptools.command.install import install as _install
+from subprocess import call
 
-version = '0.9.19a1'
+import matchbox_api_utils
+
+def _post_install(dir):
+    call([sys.executable, 'postinstall.py'])
 
 class install(_install):
     def run(self):
-        pass
-        _install.run(self)
-        self.execute(_post_install, (self.install_lib,),
-        msg="Running post install task")
+        _install.do_egg_install(self)
+        self.execute(_post_install, (self.install_lib,), msg="Running post install task")
         
-def _post_install(dir):
-    from subprocess import call
-    call([sys.executable, 'postinstall.py'])
-
 def readme():
     with open('README.rst') as fh:
         return fh.read()
@@ -24,7 +21,7 @@ config = {
     'name'                   : 'matchbox_api_utils',
     'description'            : ('MATCHBox API Utlilites Package'),
     'long_description'       : readme(),
-    'version'                : version,
+    'version'                : matchbox_api_utils.__version__,
     'author'                 : 'Dave Sims',
     'author_email'           : 'david.sims2@nih.gov',
     'download_url'           : 'https://github.com/drmrgd/matchbox_api_utils.git',
@@ -32,8 +29,7 @@ config = {
     'test_suite'             : 'nose.collector',
     'tests_require'          : ['nose'],
     'packages'               : ['matchbox_api_utils'],
-    'install_requires'       : ['setuptools', 'requests'],
-    'cmdclass'               : {'install' : install},
+    'install_requires'       : ['requests == 2.17.3'],
     'scripts'                : ['bin/map_msn_psn.py',
                                 'bin/matchbox_json_dump.py',
                                 'bin/match_specimen_tracking.py',
@@ -43,12 +39,7 @@ config = {
     'include_package_data'   : True,
     'zip_safe'               : False,
     'license'                : 'MIT',
-    'classifiers'            : [
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Development Status :: 2 - Alpha',
-    ],
+    'cmdclass'               : {'install' : install}
 }
 
 setup(**config)
