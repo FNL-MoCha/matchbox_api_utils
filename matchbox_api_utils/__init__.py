1ca35804e48643981e17bcd4c920739b1dc6cedc
Dave Sims <misc@lamneth.net>
2017-07-19 15:17:57 -0400

    Added a package variable set and updated version

--- matchbox_api_utils/__init__.py
+++ matchbox_api_utils/__init__.py
@@ -1,2 +1,10 @@
-from matchbox import Matchbox,MatchboxData
-__version__ = '0.10.1'
+import sys,os
+__all__ = ['matchbox','matchbox_conf']
+import matchbox_api_utils
+from matchbox import MatchboxData, Matchbox
+
+mb_utils_root = os.path.join(os.environ['HOME'], '.mb_utils/')
+mb_config_file = os.path.join(mb_utils_root, 'config.json')
+mb_json_data = os.path.join(mb_utils_root, 'mb_obj.json')
+
+__version__ = '0.11.0'
