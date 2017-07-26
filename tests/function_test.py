30db7363457c5c2f63c7cd67de61641e11e982a4
Dave Sims <misc@lamneth.net>
2017-07-26 13:55:41 -0400

    Updated test harness and unittests

--- tests/function_test.py
+++ tests/function_test.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
 import sys,os
-from unittest import TestCase
+import unittest
 from matchbox_api_utils import MatchData
 
-class FunctionTests(TestCase):
+class FunctionTests(unittest.TestCase):
     data = MatchData(dumped_data = os.path.join(os.path.dirname(__file__),'mb_obj.json'))
 
     def test_get_biopsy_summary(self):
+        """
+        Test get_biopsy_summary()
+
+        """
         self.assertNotEqual(self.data.get_biopsy_summary(category='failed'),0)
 
         # Total num PSNs must equal to sum(passed_biopsy,failed_biopsy,no_biopsy,outside)
@@ -19,37 +24,49 @@ class FunctionTests(TestCase):
         )
 
     def test_get_disease_summary(self):
+        """
+        Test get_disease_summary()
+
+        """
         self.assertIsNone(self.data.get_disease_summary(disease='Not a real disease'))
         # Difficult to get a real count, so just try to get greater than 0
         self.assertGreater(self.data.get_disease_summary(disease='Liposarcoma')['Liposarcoma'],0)
 
     def test_get_psn_from_msn(self):
+        """
+        Test get_psn() wiht MSN or BSN input.
+
+        """
         self.assertEqual(self.data.get_psn(msn='MSN44180'),'PSN14420')
         self.assertEqual(self.data.get_psn(msn=19349),'PSN11352')
         self.assertIsNone(self.data.get_psn(msn='6'),None)
-
-    def test_get_psn_from_bsn(self):
         self.assertEqual(self.data.get_psn(bsn='T-17-000550'),'PSN14420')
         self.assertIsNone(self.data.get_psn(bsn='no_biopsy'),None)
 
-    def test_get_msn_from_psn(self):
+    def test_get_msn(self):
+        """
+        Test get_msn() with PSN or BSN input. Also look for multiple MSN output.
+
+        """
         self.assertEqual(self.data.get_msn(psn='14420'),'MSN44180')
         self.assertEqual(self.data.get_msn(psn=11352),'MSN19349')
-
-    def test_get_msn_from_bsn(self):
         self.assertEqual(self.data.get_msn(bsn='T-17-000550'),'MSN44180')
-
-    def test_get_msn_from_psn_multiple(self):
         self.assertEqual(self.data.get_msn(psn='11926'),'MSN19992,MSN21717')
 
-    def test_get_bsn_from_psn(self):
-        self.assertEqual(self.data.get_bsn(psn='14420'),'T-17-000550')
+    def test_get_bsn(self):
+        """
+        Test get_bsn() with PSN or MSN input.
 
-    def test_get_bsn_from_msn(self):
+        """
+        self.assertEqual(self.data.get_bsn(psn='14420'),'T-17-000550')
         self.assertEqual(self.data.get_bsn(msn='44180'),'T-17-000550')
         self.assertEqual(self.data.get_bsn(msn='MSN44180'),'T-17-000550')
 
     def test_get_patients_and_disease(self):
+        """
+        Test get_patients_and_disease() with various inputs.
+
+        """
         self.assertEqual(self.data.get_patients_and_disease(psn='11352'),
             {'11352': u'Serous endometrial adenocarcinoma'})
         self.assertEqual(self.data.get_patients_and_disease(psn='11352,PSN10955,11222,PSN11070'),
@@ -74,4 +91,3 @@ class FunctionTests(TestCase):
             {'T-17-000550': u'Carcinosarcoma of the uterus'}
         )
         self.assertRaises(SystemExit,self.data.get_patients_and_disease,psn='11352',msn=3060)
-
