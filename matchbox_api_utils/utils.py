4e4eaade7e4527940cc4b204bb52711b560751bd
Dave Sims <misc@lamneth.net>
2017-07-28 13:56:27 -0400

    Added utils module with common package functions to eliminate code dupe

---
+++ matchbox_api_utils/utils.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+# Set of functions that I am commonly using across all classes.
+import re
+import json
+import datetime
+
+def load_dumped_json(json_file):
+    # Load in a JSON DB file (raw or proc) and return JSON obj and file ctime.
+    try:
+        date_string = re.search(r'.*?([0-9]+).json$',json_file).group(1)
+        formatted_date=datetime.datetime.strptime(date_string,'%m%d%y').strftime('%m/%d/%Y')
+    except (TypeError,ValueError):
+        print('got here')
+        creation_date = os.path.getctime(json_file)
+        formatted_date=datetime.datetime.fromtimestamp(creation_date).strftime('%m/%d/%Y')
+    with open(json_file) as fh:
+        return formatted_date,json.load(fh)
+
+def get_today(outtype):
+    if outtype == 'long':
+        return datetime.date.today().strftime('%m/%d/%Y')
+    elif outtype == 'short':
+        return datetime.date.today().strftime('%m%d%y')
