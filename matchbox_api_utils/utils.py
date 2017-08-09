e2638fc3b5b5a1a37ae8afa7a9229de848ff68f5
Dave Sims <misc@lamneth.net>
2017-08-09 09:56:27 -0400

    Added make_json() and get_config_data()

--- matchbox_api_utils/utils.py
+++ matchbox_api_utils/utils.py
@@ -7,6 +7,8 @@ import re
 import json
 import datetime
 
+import matchbox_conf
+
 def load_dumped_json(json_file):
     # Load in a JSON DB file (raw or proc) and return JSON obj and file ctime.
     try:
@@ -23,3 +25,11 @@ def get_today(outtype):
         return datetime.date.today().strftime('%m/%d/%Y')
     elif outtype == 'short':
         return datetime.date.today().strftime('%m%d%y')
+
+def get_config_data(config_file,item):
+    config_data = matchbox_conf.Config(config_file)
+    return config_data[item]
+
+def make_json(outfile,data):
+    with open(outfile,'w') as fh:
+        json.dump(data,fh,sort_keys=True,indent=4)
