2335fe7c6af70da761b4bb37a8220fdfb8efbbfd
Dave Sims <misc@lamneth.net>
2017-08-09 09:55:37 -0400

    Updated to query for treatment arms data

--- matchbox_api_utils/__init__.py
+++ matchbox_api_utils/__init__.py
@@ -24,7 +24,7 @@ def get_latest_data(dfiles):
     for f in dfiles:
         filename = os.path.basename(f)
         try:
-            datestring = re.search('mb_obj_([0-9]{6})\.json',filename).group(1)
+            datestring = re.search('(mb|ta)_obj_([0-9]{6})\.json',filename).group(1)
         except AttributeError:
             datestring = datetime.datetime.fromtimestamp(os.path.getctime(f)).strftime('%m%d%y')
         indexed_files[datestring] = f
@@ -34,20 +34,32 @@ def get_latest_data(dfiles):
     except IndexError:
         # there is no MATCHBox JSON Obj here from setup or whatever.
         sys.stderr.write(
-            '  -> WARN: No system default MATCHBox DB Obj location. Recommend running "matchbox_json_dump.py" and\n'
-            '           storing the resultant file in $HOME/.mb_utils/ for easier work later on.  Alternatively,\n'
+            '  -> WARN: No system default MATCHBox DB or TA obj location. Recommend running "matchbox_json_dump.py"\n'
+            '           and storing the resultant file in $HOME/.mb_utils/ for easier work later on.  Alternatively,\n'
             '           you can always do a live query.\n')
         return None
     return indexed_files[largest]
 
+def get_files(string,file_list):
+    return [x for x in file_list if os.path.basename(x).startswith(string)]
 
 json_files = [os.path.join(mb_utils_root,f) for f in os.listdir(mb_utils_root) if f.endswith('.json')]
 
-mb_data_files = []
 for f in json_files:
     if 'config.json' in f:
         mb_config_file = f
-    elif os.path.basename(f).startswith('mb_obj'):
-        mb_data_files.append(f)
 
+mb_data_files = get_files('mb_obj',json_files)
 mb_json_data = get_latest_data(mb_data_files)
+# print('mb JSON datafile is: %s' % mb_json_data)
+# sys.exit()
+
+ta_data_files = get_files('ta_obj',json_files)
+ta_json_data = get_latest_data(ta_data_files)
+# print('TA JSON datafile is: %s' % ta_json_data)
+# sys.exit()
+
+amois_files = get_files('amoi_lookup',json_files)
+amoi_json_data = get_latest_data(amois_files)
+# print('amois Lookup JSON datafile is: %s' % amoi_json_data)
+# sys.exit()
