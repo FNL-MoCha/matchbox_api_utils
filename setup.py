687687cc2fe73e9fb50cdcb25bef548cf2889db3
Dave Sims <misc@lamneth.net>
2017-08-09 10:46:05 -0400

    v0.14.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools.command.install import install as _install
 from subprocess import call
 
-version = '0.14.0'
+version = '0.14.1'
 
 def _post_install(dir):
     call([sys.executable, 'postinstall.py'])
