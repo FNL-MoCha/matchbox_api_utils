d43e9326267142c7e42156e3a7d7e08416acd090
Dave Sims <misc@lamneth.net>
2017-08-10 09:06:23 -0400

    removed config class calls to integrate better with current package version

--- bin/match_variant_frequency.py
+++ bin/match_variant_frequency.py
@@ -9,28 +9,7 @@ from pprint import pprint as pp
 
 from matchbox_api_utils import MatchData
 
-version = '0.10.1_071317'
-
-class Config(object):
-    def __init__(self,config_file):
-        self.config_file = config_file
-        self.config_data = Config.read_config(self.config_file)
-
-    def __repr__(self):
-        return '%s:%s' % (self.__class__,self.__dict__)
-
-    def __getitem__(self,key):
-        return self.config_data[key]
-
-    def __iter__(self):
-        return self.config_data.itervalues()
-
-    @classmethod
-    def read_config(cls,config_file):
-        '''Read in a config file of params to use in this program'''
-        with open(config_file) as fh:
-            data = json.load(fh)
-        return data
+version = '0.11.0_081017'
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -113,18 +92,6 @@ def split_genes(x):
     return [y.upper() for y in x.split(',')]
 
 if __name__=='__main__':
-    config_file = os.path.join(os.environ['HOME'], '.mb_utils/config.json')
-    if not os.path.isfile(config_file):
-        config_file = os.path.join(os.getcwd(), 'config.json')
-
-    try:
-        config_data = Config.read_config(config_file)
-    except IOError:
-        sys.stderr.write('ERROR: No configuration file found! Need to create a '
-            'config file in the current directory or use the system provided one '
-            'in ~/.mb_utils/config.json (preferred).\n')
-        sys.exit(1)
-
     args = get_args()
 
     query_list = {}
@@ -154,7 +121,7 @@ if __name__=='__main__':
     if not args.json:
         sys.stdout.write('Retrieving MATCHBox data object.  This will take a few minutes...')
         sys.stdout.flush()
-    data = MatchData(config_data['url'],config_data['creds'],dumped_data=args.json)
+    data = MatchData(dumped_data=args.json)
     sys.stdout.write('\n')
 
     # Gen a query result
