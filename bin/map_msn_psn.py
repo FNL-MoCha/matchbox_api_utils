c36382c0a3feefb5e74f81d435589b5bfd9dbd77
Dave Sims <misc@lamneth.net>
2018-02-12 15:43:33 -0500

    Fixed output file handle bug.

--- bin/map_msn_psn.py
+++ bin/map_msn_psn.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Input a MSN, BSN, or PSN, and return the other identifiers. Useful when trying to retrieve the 
-correct dataset and you only know one piece of information.
+Input a MSN, BSN, or PSN, and return the other identifiers. Useful when trying to 
+retrieve the correct dataset and you only know one piece of information.
 
-Note: We are only working with internal BSN, MSN, and PSN numbers for now and can not return
-Outside Assay identifiers at this time. 
+Note: We are only working with internal BSN, MSN, and PSN numbers for now and can 
+not return Outside Assay identifiers at this time. 
 """
 import sys
 import os
@@ -17,30 +17,32 @@ from pprint import pprint as pp
 
 from matchbox_api_utils import MatchData 
 
-version = '3.2.012618'
+version = '3.3.021218'
 
 def get_args():
     parser = argparse.ArgumentParser(description = __doc__)
     parser.add_argument('ids', metavar='<IDs>', nargs='?',
-            help='MATCH IDs to query.  Can be single or comma separated list. '
-                'Must be used with PSN or MSN option.')
-    parser.add_argument('-j', '--json', metavar='<mb_json_file>', default='sys_default',
-            help='Load a MATCHBox JSON file derived from "matchbox_json_dump.py" instead '
-                 'of a live query. By default will load the "sys_default" created during '
-                 'package installation. If you wish to do a live query (i.e. not load a '
-                 'previously downloaded JSON dump, set -j to "None".')
-    parser.add_argument('-t', '--type', choices=['psn','msn','bsn'], required=True, type=str.lower,
-            help='Type of query string input. Can be MSN, PSN, or BSN')
+        help='MATCH IDs to query.  Can be single or comma separated list. '
+        'Must be used with PSN or MSN option.')
+    parser.add_argument('-j', '--json', metavar='<mb_json_file>', 
+        default='sys_default', help='Load a MATCHBox JSON file derived from '
+        '"matchbox_json_dump.py" instead of a live query. By default will '
+        'load the "sys_default" created during package installation. If you '
+        'wish to do a live query (i.e. not load a previously downloaded JSON '
+        'dump, set -j to "None".')
+    parser.add_argument('-t', '--type', choices=['psn','msn','bsn'], 
+        required=True, type=str.lower, help='Type of query string input. Can '
+        'be MSN, PSN, or BSN')
     parser.add_argument('-f', '--file', metavar="<input_file>", 
-            help='Load a batch file of all MSNs or PSNs to proc')
-    parser.add_argument('-o', '--outfile', metavar='<outfile>', default=sys.stdout,
-        help='Write output to file.')
+        help='Load a batch file of all MSNs or PSNs to proc')
+    parser.add_argument('-o', '--outfile', metavar='<outfile>', help='File to '
+        'which output should be written. Default: STDOUT.')
     parser.add_argument('-v','--version', action='version', 
         version = '%(prog)s  -  ' + version)
     args = parser.parse_args()
 
-    # Kludy way to use a sys default for the API, while still allowing for live queries
-    # if need be.  Probably a better way, but whatever!
+    # Kludy way to use a sys default for the API, while still allowing for live 
+    # queries if need be.  Probably a better way, but whatever!
     if args.json == 'None':
         args.json = None
     return args
@@ -85,36 +87,12 @@ def map_id(mb_data, id_list, qtype):
                 else:
                     msn = '---'
                 results.append((psn, pt, msn))
-
-        # TODO: Need to handle errors better.  this won't work for outside assays. 
-        #       For example, looking up 16435 returns:
-        #
-        #            [u'61b685c6-8a72-43b2-84cc-b5c1abea41eb', u'MSN61978']
-        #
-        #       Perfectly valid data, but not that we can rely on fixing.
-        # print(','.join(return_val))
-
-        #if return_val[1].startswith('T-1') and return_val[2].startswith('MSN'):
-            #print(','.join(return_val))
-        #else:
-            #print('error')
-
-        # if not any(x.startswith('T-1') for x in return_val[1]):
-            # print('got here')
-            # sys.stderr.write("Can not find a match for non-outside assay results for input: %s\n" % pt)
-            # return None
-        # elif not any(x.startswith('MSN') for x in return_val[2]):
-            # sys.stderr.write("Can not find a match for non-outside assay results for input: %s\n" % pt)
-            # return None
-        # else:
-            # print(','.join(return_val))
     return results
 
 def cat_list(l):
     return ';'.join(l)
 
-def print_results(data, output_file):
-    outfh = open(output_file, 'w')
+def print_results(data, outfh):
     if data:
         sys.stdout.write('\n') # pad output from stderr msg if printing to stdout
         outfh.write(','.join(['PSN', 'BSN', 'MSN']))
@@ -143,7 +121,8 @@ def validate_list(id_list,qtype):
             trimmed = re.search(type_regex[qtype],elem).group(1)
             valid_list.append(trimmed)
         except AttributeError:
-            sys.stdout.write("WARN: id '{}' is not valid. Skipping entry!\n".format(elem))
+            sys.stdout.write("WARN: id '{}' is not valid. Skipping "
+                "entry!\n".format(elem))
     return valid_list
 
 if __name__=='__main__':
@@ -172,4 +151,8 @@ if __name__=='__main__':
     print('Getting MSN / PSN mapping data...')
     results = map_id(data, valid_ids, args['type'])
 
-    print_results(results, args['outfile'])
+    if args['outfile']:
+        outfh = open(args['outfile'], 'w')
+    else:
+        outfh = sys.stdout
+    print_results(results, outfh)
