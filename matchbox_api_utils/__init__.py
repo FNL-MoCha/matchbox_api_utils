afef317e26e22bd08aa5fd7ba4932b2db4512642
Dave Sims <misc@lamneth.net>
2018-02-14 17:23:11 -0500

    Making the root dir in __init__ now since we need this present when we run the setup script (though it is made by the postinstaller). Maybe there is a better way, but this should work OK for now.

--- matchbox_api_utils/__init__.py
+++ matchbox_api_utils/__init__.py
@@ -8,11 +8,14 @@ from matchbox_api_utils.matchbox import Matchbox
 from matchbox_api_utils.match_data import MatchData
 from matchbox_api_utils.match_arms import TreatmentArms
 
-__version__ = '1.0.021318'
+__version__ = '1.2.021418'
 
 __all__ = ['Matchbox','MatchData','TreatmentArms','matchbox_conf']
 
-mb_utils_root = os.path.join(os.environ['HOME'], '.mb_utils/')
+mb_utils_root = os.path.join(os.environ['HOME'], '.mb_utils')
+if not os.path.isdir(mb_utils_root):
+    os.mkdir(mb_utils_root)
+
 
 def get_latest_data(dfiles):
     """
@@ -25,7 +28,8 @@ def get_latest_data(dfiles):
     for f in dfiles:
         filename = os.path.basename(f)
         try:
-            datestring = re.search('(mb|ta)_obj_([0-9]{6})\.json',filename).group(1)
+            datestring = re.search('(mb|ta)_obj_([0-9]{6})\.json', 
+                filename).group(1)
         except AttributeError:
             datestring = datetime.datetime.fromtimestamp(os.path.getctime(f)).strftime('%m%d%y')
         indexed_files[datestring] = f
@@ -35,16 +39,20 @@ def get_latest_data(dfiles):
     except IndexError:
         # there is no MATCHBox JSON Obj here from setup or whatever.
         sys.stderr.write(
-            '  -> WARN: No system default MATCHBox DB or TA obj location. Recommend running "matchbox_json_dump.py"\n'
-            '           and storing the resultant file in $HOME/.mb_utils/ for easier work later on.  Alternatively,\n'
-            '           you can always do a live query.\n')
+        """
+        -> WARN: No system default MATCHBox DB of TA obj location! Recommend
+           running "matchbox_json_dump.py" and storing the resultant file in 
+           $HOME/.mb_utils/ for easier work later on. Alternatively, you can 
+           always do a live query.
+        """
+        )
         return None
     return indexed_files[largest]
 
 def get_files(string,file_list):
     return [x for x in file_list if os.path.basename(x).startswith(string)]
 
-json_files = [os.path.join(mb_utils_root,f) for f in os.listdir(mb_utils_root) if f.endswith('.json')]
+json_files = [os.path.join(mb_utils_root, f) for f in os.listdir(mb_utils_root) if f.endswith('.json')]
 
 for f in json_files:
     if 'config.json' in f:
