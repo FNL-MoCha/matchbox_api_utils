c29feee5f4b481d88a586fd7ccf2c3326963db95
Dave Sims <misc@lamneth.net>
2018-02-27 16:46:41 -0500

    Updated the docstrings

--- bin/matchbox_json_dump.py
+++ bin/matchbox_json_dump.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Get parsed dataset from MATCHBox and dump as a JSON object that we can use 
+later on to speed up development and periodic searching for data.  
+"""
 import sys
 import os
 import json
@@ -13,24 +17,15 @@ from matchbox_api_utils import TreatmentArms
 version = '3.0.0_080917'
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        formatter_class = lambda prog: 
-            argparse.HelpFormatter(prog, max_help_position=100, width=125),
-        description=
-        '''
-        Get parsed dataset from MATCHBox and dump as a JSON object that we can use 
-        later on to speed up development and periodic searching for data.  
-        '''
-    )
-    # TODO: get rid of this?
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-d', '--data', metavar='<raw_mb_datafile.json>',
-        help='Load a raw MATCHBox database file (usually after running with the -r option).')
+        help='Load a raw MATCHBox database file (usually after running with the '
+        '-r option).')
     parser.add_argument('-r', '--raw', action='store_true',
         help='Generate a raw dump of MATCHbox for debugging and dev purposes.')
     parser.add_argument('-p', '--patient', metavar='<psn>', 
-        help='Patient sequence number used to limit output for testing and dev purposes')
-    # TODO: get rid of this?
-
+        help='Patient sequence number used to limit output for testing and dev '
+        'purposes')
     parser.add_argument('-t', '--ta_json' , metavar='<ta_obj.json>',
         help='Treatment Arms obj JSON filename. DEFAULT: ta_obj_<datestring>.json')
     parser.add_argument('-a', '--amoi_json' , metavar='<amoi_obj.json>',
@@ -44,12 +39,14 @@ def get_args():
     return args
 
 def main(data,arms,mb_filename=None,ta_filename=None,amois_filename=None):
-    sys.stdout.write('Dumping matchbox as a JSON file for easier and faster code testing...')
+    sys.stdout.write('Dumping matchbox as a JSON file for easier and faster code '
+    'testing...')
     sys.stdout.flush()
     data.matchbox_dump(filename=mb_filename)
     sys.stdout.write('Done!\n')
 
-    sys.stdout.write('Dumping Treatment Arms as a JSON file for easier and faster code testing...')
+    sys.stdout.write('Dumping Treatment Arms as a JSON file for easier and faster '
+    'code testing...')
     sys.stdout.flush()
     arms.ta_json_dump(amois_filename=amois_filename,ta_filename=ta_filename)
     sys.stdout.write("Done!\n")
@@ -58,7 +55,8 @@ if __name__=='__main__':
     args = get_args()
 
     if args.raw:
-        sys.stdout.write('\n*** Making a raw dump of MATCHBox for dev / testing purposes ***\n')
+        sys.stdout.write('\n*** Making a raw dump of MATCHBox for dev / testing '
+        'purposes ***\n')
         sys.stdout.flush()
         MatchData(make_raw=True)
         sys.stdout.write('Done!\n')
