2177e67193d37e7650f2544da934d66bd5643a68
Dave Sims <misc@lamneth.net>
2018-04-11 15:09:59 -0400

    Removed the config file handling. this was supposed to get handled by matchbox_conf

--- matchbox_api_utils/utils.py
+++ matchbox_api_utils/utils.py
@@ -28,10 +28,9 @@ def get_today(outtype):
     elif outtype == 'short':
         return datetime.date.today().strftime('%m%d%y')
 
-def get_config_data(config_file,item):
-    config_data = matchbox_conf.Config(config_file)
-    return config_data[item]
+def make_json(outfile, data, sort=True):
+    with open(outfile, 'w') as fh:
+        json.dump(data, fh, sort_keys=sort, indent=4)
 
-def make_json(outfile,data):
-    with open(outfile,'w') as fh:
-        json.dump(data, fh, sort_keys=True, indent=4)
+def print_json(data):
+    return json.dumps(data, sort_keys=True, indent=4)
