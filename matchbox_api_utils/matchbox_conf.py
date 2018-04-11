85899c76f74fb1aa389614762834ab817ef70c15
Dave Sims <misc@lamneth.net>
2018-04-11 15:09:15 -0400

    Centralized the config file reading and handling here as it should be (utils was taking up too much slack).

--- matchbox_api_utils/matchbox_conf.py
+++ matchbox_api_utils/matchbox_conf.py
@@ -5,10 +5,13 @@ import json
 
 import matchbox_api_utils
 
+from pprint import pprint as pp
+from matchbox_api_utils import utils
+
 
 class Config(object):
-    def __init__(self, mb_config_file=None, mb_json_data=None, ta_json_data=None,
-            amois_lookup=None):
+    def __init__(self, matchbox_name, config_file=None, mb_json_data=None, 
+            ta_json_data=None, amois_lookup=None):
         """
         MATCHBox Configuration Class
 
@@ -17,12 +20,15 @@ class Config(object):
 
         """
 
-        if mb_config_file:
-            self.config_file = mb_config_file
-        else:
-            self.config_file = matchbox_api_utils.mb_config_file
-        self.config_data = Config.read_config(self.config_file)
+        self._matchbox_name = matchbox_name
+        if config_file is not None:
+            self._config_file = config_file
+        else: 
+            self._config_file = matchbox_api_utils.mb_config_file
+        self.config_data = self.read_config()
 
+        # TODO: Fix this 
+        '''
         if mb_json_data:
             self.config_data['mb_json_data'] = mb_json_data
         else:
@@ -37,25 +43,30 @@ class Config(object):
             self.config_data['amois_lookup'] = amois_lookup
         else:
             self.config_data['amois_lookup'] = matchbox_api_utils.amoi_json_data
-
+        '''
 
     def __repr__(self):
-        return '%s:%s' % (self.__class__,self.__dict__)
+        return '%s:%s' % (self.__class__, self.__dict__)
 
-    def __getitem__(self,key):
+    def __str__(self):
+        return utils.print_json(self.config_data)
+
+    def __getitem__(self, key):
         return self.config_data[key]
 
     def __iter__(self):
         return self.config_data.itervalues()
 
-    @classmethod
-    def read_config(cls,config_file):
+    def get_config_item(self, item):
+        return self.config_data[item]
+
+    def read_config(self):
         try:
-            with open(config_file) as fh:
+            with open(self._config_file) as fh:
                 data = json.load(fh)
         except IOError:
             sys.stderr.write('ERROR: No configuration file found. You must either '
                 'run the package configuration tools or provide a config file '
                 'using the "config" option. Can not continue!\n')
             sys.exit(1)
-        return data
+        return data[self._matchbox_name]
