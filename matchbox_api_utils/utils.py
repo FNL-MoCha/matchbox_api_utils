83e92ccbcd039e3f229b59a19675dfd905883afc
Dave Sims <misc@lamneth.net>
2018-04-17 15:15:08 -0400

    Added some new handy methods.

--- matchbox_api_utils/utils.py
+++ matchbox_api_utils/utils.py
@@ -6,6 +6,9 @@ import re
 import json
 import datetime
 
+from termcolor import colored, cprint
+from pprint import pprint as pp
+
 from matchbox_api_utils import matchbox_conf
 
 
@@ -24,13 +27,32 @@ def load_dumped_json(json_file):
 
 def get_today(outtype):
     if outtype == 'long':
-        return datetime.date.today().strftime('%m/%d/%Y')
+        # return datetime.date.today().strftime('%m/%d/%Y')
+        return datetime.date.today().strftime('%Y-%m-%d')
     elif outtype == 'short':
         return datetime.date.today().strftime('%m%d%y')
 
-def make_json(outfile, data, sort=True):
+def epoch_to_hr_date(epoch_date):
+    return datetime.datetime.fromtimestamp(epoch_date).strftime('%Y-%m-%d')
+
+def get_vals(d, *v):
+    # return a value or list of values
+    return [d.get(i, '---') for i in v]
+
+def make_json(*, outfile, data, sort=True):
     with open(outfile, 'w') as fh:
         json.dump(data, fh, sort_keys=sort, indent=4)
 
 def print_json(data):
     return json.dumps(data, sort_keys=True, indent=4)
+
+def read_json(json_file):
+    with open(json_file) as fh:
+        return json.load(fh)
+
+def __exit__(line, msg=None):
+    output = ('Script stopped at line: {} with message: "{}".'.format(line, msg))
+    sys.stderr.write('\n')
+    cprint(output, 'white', 'on_green', attrs=['bold'], file=sys.stderr)
+    sys.exit()
+
