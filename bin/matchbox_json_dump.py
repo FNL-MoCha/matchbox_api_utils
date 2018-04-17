8e8e37ff5aee8ee7390debe21936627115a53ea4
Dave Sims <misc@lamneth.net>
2018-04-17 15:41:40 -0400

    Updated to handle v2.0 data and to create new files.

--- bin/matchbox_json_dump.py
+++ bin/matchbox_json_dump.py
@@ -14,13 +14,18 @@ import matchbox_api_utils
 from matchbox_api_utils import MatchData
 from matchbox_api_utils import TreatmentArms
 
-version = '3.0.0_080917'
+version = '3.1.041718'
 
 def get_args():
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('matchbox', metavar='<matchbox>', 
+        default='adult-matchbox',
+        help='Name of MATCHBox to which we make the file. Valid systems are: '
+        '"adult-matchbox", "adult-matchbox-uat", "ped-matchbox". '
+        'DEFAULT: %(default)s')
     parser.add_argument('-d', '--data', metavar='<raw_mb_datafile.json>',
-        help='Load a raw MATCHBox database file (usually after running with the '
-        '-r option).')
+        help='Load a raw MATCHBox database file (usually after running with '
+            'the -r option).')
     parser.add_argument('-r', '--raw', action='store_true',
         help='Generate a raw dump of MATCHbox for debugging and dev purposes.')
     parser.add_argument('-p', '--patient', metavar='<psn>', 
@@ -38,34 +43,36 @@ def get_args():
     args = parser.parse_args()
     return args
 
-def main(data,arms,mb_filename=None,ta_filename=None,amois_filename=None):
-    sys.stdout.write('Dumping matchbox as a JSON file for easier and faster code '
-    'testing...')
+def main(data, arms, mb_filename=None, ta_filename=None, amois_filename=None):
+    sys.stdout.write('Dumping matchbox as a JSON file for easier and faster '
+        'code testing...')
     sys.stdout.flush()
     data.matchbox_dump(filename=mb_filename)
     sys.stdout.write('Done!\n')
 
-    sys.stdout.write('Dumping Treatment Arms as a JSON file for easier and faster '
-    'code testing...')
+    sys.stdout.write('Dumping Treatment Arms as a JSON file for easier and '
+        'faster code testing...')
     sys.stdout.flush()
-    arms.ta_json_dump(amois_filename=amois_filename,ta_filename=ta_filename)
+    arms.ta_json_dump(amois_filename=amois_filename, ta_filename=ta_filename)
     sys.stdout.write("Done!\n")
 
 if __name__=='__main__':
     args = get_args()
 
     if args.raw:
-        sys.stdout.write('\n*** Making a raw dump of MATCHBox for dev / testing '
-        'purposes ***\n')
+        sys.stdout.write('\n*** Making a raw dump of MATCHBox (%s) for dev and '
+            'testing purposes ***\n' % args.matchbox)
         sys.stdout.flush()
-        MatchData(make_raw=True)
+        MatchData(matchbox=args.matchbox, make_raw=True)
         sys.stdout.write('Done!\n')
         sys.exit()
 
-    sys.stdout.write('Retrieving data from MATCHBox...')
+    sys.stdout.write('\nRetrieving data from MATCHBox (%s)...' % args.matchbox)
     sys.stdout.flush()
-    data = MatchData(json_db=None,load_raw=args.data,patient=args.patient)
-    arms = TreatmentArms(json_db=None)
+
+    data = MatchData(matchbox=args.matchbox, json_db=None, load_raw=args.data,
+        patient=args.patient)
+    arms = TreatmentArms(matchbox=args.matchbox, json_db=None)
     sys.stdout.write('Done!\n')
 
-    main(data,arms,args.mb_json,args.ta_json,args.amoi_json)
+    main(data, arms, args.mb_json, args.ta_json, args.amoi_json)
