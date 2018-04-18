776d4622e99c05b6b0b650751ee506043d9ef1f9
Dave Sims <misc@lamneth.net>
2018-04-18 14:28:42 -0400

    Added a method to map medra and histology, which I think is necessary from time to time in all of these modules.

--- matchbox_api_utils/utils.py
+++ matchbox_api_utils/utils.py
@@ -39,6 +39,24 @@ def get_vals(d, *v):
     # return a value or list of values
     return [d.get(i, '---') for i in v]
 
+def map_histology(data, *, medra=None, histology=None):
+    if medra:
+        ctep_name = data.get(medra, None)
+        if ctep_name is None:
+            sys.stderr.write('No disease with MEDRA code "%s" was found in '
+                'the MATCH dataset.\n')
+            return None
+        else:
+            return ctep_name
+    elif histology:
+        for key, val in data.items():
+            if val == histology:
+                # OK to just assign it here, since I know the codes have to 
+                # be unique
+                return key
+    else:
+        return data
+                    
 def make_json(*, outfile, data, sort=True):
     with open(outfile, 'w') as fh:
         json.dump(data, fh, sort_keys=sort, indent=4)
