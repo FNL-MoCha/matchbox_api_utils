26a1fa01c4f715b78613cd8d74be16603b475471
Dave Sims <misc@lamneth.net>
2018-08-09 12:19:28 -0400

    v2.5.080918: Changed authetication headers for Auth0 based on MATCHBox team requirements.

--- matchbox_api_utils/matchbox.py
+++ matchbox_api_utils/matchbox.py
@@ -175,7 +175,7 @@ class Matchbox(object):
             "username" : self._username,
             "password" : self._password,
             "grant_type" : "password",
-            "scope" : "openid roles email",
+            "profile" : "openid roles email profile",
             "connection" : self._client_name,
         }
         url = 'https://ncimatch.auth0.com/oauth/ro'
